38561526cc6b35790dba162324b4ca10bede6b5d
Mojo213 <104395212+Mojo213@users.noreply.github.com>
2024-08-27 20:23:26 +0100

    add vertical ship placement test.

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,7 +1,7 @@
 const placeShipOnBoard = require('../src/gameboard');
 
 describe('gameBoard', () => {
-test('place ship on board horizontal', () => {
+test('place ship on board horizontally', () => {
     const rows = 10;
     const columns = 10;
     const arrayBoard = Array.from(Array(rows), () => new Array(columns));
@@ -18,6 +18,23 @@ test('place ship on board horizontal', () => {
     placeShipOnBoard(arrayBoard, row, column, shipLength, orientation);
     expect(arrayBoard).toEqual(expectedBoard);
 });
+test('place ship on board vertically', () => {
+    const rows = 10;
+    const columns = 10;
+    const arrayBoard = Array.from(Array(rows), () => new Array(columns));
+    let row = 2;
+    let column = 0
+    let shipLength = 3;
+    let orientation = 'vertical'
 
+    const expectedBoard = Array.from(Array(rows), () => new Array(columns));
+    expectedBoard[2][0] = 'ship';
+    expectedBoard[3][0] = 'ship';
+    expectedBoard[4][0] = 'ship';
+
+    placeShipOnBoard(arrayBoard, row, column, shipLength, orientation);
+    expect(arrayBoard).toEqual(expectedBoard);
+
+});
 
 })
