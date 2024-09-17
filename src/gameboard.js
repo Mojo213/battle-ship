c510ecac2201a3b73378d0b74fca740f8ae117d9
Mojo213 <104395212+Mojo213@users.noreply.github.com>
2024-09-17 21:17:22 +0100

    create gameboard factory func get board func missed attacks func return relevant functions

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,49 +1,63 @@
-const rows = 10;
-const columns = 10;
-// const arrayBoard = Array.from(Array(rows), () => new Array(columns));
+function Gameboard() {
+    const rows = 10;
+    const columns = 10;
+    let arrayBoard = Array.from(Array(rows), () => new Array(columns));
+    let missedAttacks = [];
 
-// let row = 2;
-// let column = 0
-// let shipLength = 3;
-// let orientation = 'vertical'
-
-function placeShipOnBoard(arrayBoard, ship, row, column, orientation) {
-    for (let i = 0; i < ship.length; i++) {
-      if (orientation === 'horizontal') {
-        if (column + i < columns) {  
-          arrayBoard[row][column + i] = { ship: ship, part: i };
-        } else {
-          return "Ship can't be placed out of bounds!";
+    function placeShipOnBoard(ship, row, column, orientation) {
+        for (let i = 0; i < ship.length; i++) {
+            if (orientation === 'horizontal') {
+                if (column + i < columns) {
+                    arrayBoard[row][column + i] = { ship: ship, part: i };
+                } else {
+                    return "Ship can't be placed out of bounds!";
+                }
+            } else if (orientation === 'vertical') {
+                if (row + i < rows) {
+                    arrayBoard[row + i][column] = { ship: ship, part: i };
+                } else {
+                    return "Ship can't be placed out of bounds!";
+                }
+            }
         }
-      } else if (orientation === 'vertical') {
-        if (row + i < rows) {
-          arrayBoard[row + i][column] = { ship: ship, part: i };
-        } else {
-          return "Ship can't be placed out of bounds!";
+        return arrayBoard;
+    }
+
+    function receiveAttack(hitRow, hitColumn) {
+        let cell = arrayBoard[hitRow][hitColumn];
+
+        if (cell && cell.hit) {
+            return 'This part of the ship has already been hit.';
         }
-      }
+        if (cell && cell.ship) {
+            cell.ship.hit();
+            arrayBoard[hitRow][hitColumn] = { ...cell, hit: true };
+            return 'Ship has been hit';
+        } else {
+            missedAttacks.push({ hitRow, hitColumn });
+            return 'Missed the ship';
+        }
     }
-    return arrayBoard; 
-  }
 
-
-  function receiveAttack(board, hitRow, hitColumn) {
-    let cell = board[hitRow][hitColumn];
-
-    if (cell && cell.hit) {
-        return 'This part of the ship has already been hit.';
+    function getMissedAttacks() {
+        return missedAttacks;
     }
-    if (cell && cell.ship) { 
-        cell.ship.hit();
-        board[hitRow][hitColumn] = { ...cell, hit: true };  
-        return 'ship has been hit';
-    } else {
-        return 'Missed the ship';
+
+    function getBoard() {
+        return arrayBoard;
     }
+
+    return {
+        placeShipOnBoard,
+        receiveAttack,
+        getMissedAttacks,
+        getBoard
+    };
 }
 
 
-module.exports = {placeShipOnBoard, receiveAttack};
+
+module.exports = Gameboard;
     
   
 
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,61 +1,61 @@
-const {placeShipOnBoard, receiveAttack } = require('../src/gameboard');
-const createShip = require('../src/ship');
+const Gameboard = require('../src/gameboard'); 
+const createShip = require('../src/ship'); 
 
-
-describe('gameBoard', () => {
-    const rows = 10;
-    const columns = 10;
-    let arrayBoard;
-    let row = 2;
-    let column = 0;
-    let ship = createShip(3); 
+describe('Gameboard', () => {
+    let gameBoard;
+    let ship = createShip(3);
+    const row = 2;
+    const column = 0;
 
     beforeEach(() => {
-        arrayBoard = Array.from(Array(rows), () => new Array(columns)); 
+        gameBoard = Gameboard();
     });
 
     test('place ship on board horizontally', () => {
-        let orientation = 'horizontal';
+        const orientation = 'horizontal';
 
-        const expectedBoard = Array.from(Array(rows), () => new Array(columns));
+        gameBoard.placeShipOnBoard(ship, row, column, orientation);
+
+        const expectedBoard = Array.from(Array(10), () =>  Array(10));
         expectedBoard[2][0] = { ship, part: 0 };
         expectedBoard[2][1] = { ship, part: 1 };
         expectedBoard[2][2] = { ship, part: 2 };
 
-        placeShipOnBoard(arrayBoard, ship, row, column, orientation);
-        expect(arrayBoard).toEqual(expectedBoard);
+        expect(gameBoard.getBoard()).toEqual(expectedBoard);
     });
 
     test('place ship on board vertically', () => {
-        let orientation = 'vertical';
+        const orientation = 'vertical';
 
-        const expectedBoard = Array.from(Array(rows), () => new Array(columns));
+        gameBoard.placeShipOnBoard(ship, row, column, orientation);
+
+        const expectedBoard = Array.from(Array(10), () =>  Array(10));
         expectedBoard[2][0] = { ship, part: 0 };
         expectedBoard[3][0] = { ship, part: 1 };
         expectedBoard[4][0] = { ship, part: 2 };
 
-        placeShipOnBoard(arrayBoard, ship, row, column, orientation);
-        expect(arrayBoard).toEqual(expectedBoard);
+        expect(gameBoard.getBoard()).toEqual(expectedBoard);
     });
 
-    test('ship has been hit', () =>{
-        placeShipOnBoard(arrayBoard, ship, row, column, 'horizontal')
-        receiveAttack(arrayBoard, row, column);
-        let cell = arrayBoard[row][column];
-        expect(cell).toEqual({...cell, hit: true});
-    })
-    test('This part of the ship has already been hit.', () =>{
-        placeShipOnBoard(arrayBoard, ship, row, column, 'horizontal');
-        receiveAttack(arrayBoard, row, column);
-        let result = receiveAttack(arrayBoard, row, column);
-        expect(result).toBe('This part of the ship has already been hit.');
-    })
+    test('ship has been hit', () => {
+        gameBoard.placeShipOnBoard(ship, row, column, 'horizontal');
+        gameBoard.receiveAttack(row, column);
+        const cell = gameBoard.getBoard()[row][column];
+        expect(cell).toEqual({ ship, part: 0, hit: true });
+    });
 
-    test('Missed the ship', () =>{
-        placeShipOnBoard(arrayBoard, ship, row, column, 'horizontal');
-        receiveAttack(arrayBoard, 1, 0);
-        let result = receiveAttack(arrayBoard, 1, 0);
+    test('This part of the ship has already been hit.', () => {
+        gameBoard.placeShipOnBoard(ship, row, column, 'horizontal');
+        gameBoard.receiveAttack(row, column);
+        const result = gameBoard.receiveAttack(row, column);
+        expect(result).toBe('This part of the ship has already been hit.');
+    });
+
+    test('Missed the ship', () => {
+        gameBoard.placeShipOnBoard(ship, row, column, 'horizontal');
+        gameBoard.receiveAttack(1, 0);
+        const result = gameBoard.receiveAttack(1, 0);
         expect(result).toBe('Missed the ship');
-    })
+    });
 
 });
