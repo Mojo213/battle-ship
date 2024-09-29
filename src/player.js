3aa5e881cd2211204ef6f6551bab05253953f951
Mojo213 <104395212+Mojo213@users.noreply.github.com>
2024-09-29 23:24:20 +0100

    plan

--- src/player.js
+++ src/player.js
@@ -1,9 +1,10 @@
 const Gameboard = require('../src/gameboard'); 
 
-function player (name){
-
-    return{
-        name, 
-        let board = Gameboard(),
-    }
+function player (name, opponent){
+    let board = Gameboard()
+  
 }
+
+// create a player function that takes the players name in this instance computer or player and first 
+// give them the ability to have their own board the abilty to be attacked the ability to place ships
+// abilty the have access to their own gameboard and all the gameboards methods and functionality 
