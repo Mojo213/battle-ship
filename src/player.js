7f9baa8b35a97a68179ca749a5d23e16928e1034
Mojo213 <104395212+Mojo213@users.noreply.github.com>
2024-09-30 23:39:20 +0100

    adjust row column gen

--- src/player.js
+++ src/player.js
@@ -1,24 +1,30 @@
 const Gameboard = require('../src/gameboard'); 
+let attackedCoordinates = [];
 
 function player (name, opponent){
     let board = Gameboard()
     return{
         name, name,
-        playerBoard: board.getBoard,
-        playerAttack: 
+        playerBoard: board.getBoard(),
+        attack(opponent, row, column) {
+            return opponent.receiveAttack(row, column);
+        },
+        computerAttack: computerAttack(),
         
     }
 }
 
-function rowGenerator () {
-let  hitRow = Math.floor(Math.random() * 11); 
 
- return hitRow
+
+
+function rowGenerator (){
+let hitRow = Math.floor(Math.random() * 10); 
+return hitRow
 }
 
-function columnGenerator(){
-    let hitColumn = Math.floor(Math.random() * 11);
-    return hitColumn;
+function columnGenerator (){
+let hitColumn = Math.floor(Math.random() * 10);
+return hitColumn;
 }
 
 // create a player function that takes the players name in this instance computer or player and first 
