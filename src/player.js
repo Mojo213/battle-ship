194d98b05d95661f441a4bec0805ebb7587554b6
Mojo213 <104395212+Mojo213@users.noreply.github.com>
2024-10-02 03:18:48 +0100

    adjust player factory function. add attack and placeShip functionality

--- src/player.js
+++ src/player.js
@@ -1,18 +1,15 @@
 const Gameboard = require('../src/gameboard'); 
-let attackedCoordinates = [];
 
-function player (name, opponent){
-    let board = Gameboard()
-    return{
-        name, name,
-        playerBoard: board.getBoard(),
-        attack(opponent, row, column) {
-            return opponent.receiveAttack(row, column);
-        },
-        computerAttack: computerAttack(),
-        
+function player(name) {
+    let board = Gameboard();
+    return {
+        name,
+        placeShip: (ship, row, column, orientation) => board.placeShipOnBoard(ship, row, column, orientation),
+        attack: (opponent, row, column) => opponent.receiveAttack(row, column), 
+        getBoard: () => board.getBoard(),
     }
 }
+
 
 
 
@@ -27,6 +24,3 @@ let hitColumn = Math.floor(Math.random() * 10);
 return hitColumn;
 }
 
-// create a player function that takes the players name in this instance computer or player and first 
-// give them the ability to have their own board the abilty to be attacked the ability to place ships
-// abilty the have access to their own gameboard and all the gameboards methods and functionality 
