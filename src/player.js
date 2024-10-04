4ce8d5327aa915b7dc7a1def2864b95bc998e847
Mojo213 <104395212+Mojo213@users.noreply.github.com>
2024-10-04 04:02:41 +0100

    enhance attack method to include computer attack

--- src/player.js
+++ src/player.js
@@ -1,18 +1,29 @@
 const Gameboard = require('../src/gameboard'); 
 
-function player(name) {
+function player(name, isComputer = false) {
     let board = Gameboard();
+    let attacks = new Set();
     return {
         name,
         placeShip: (ship, row, column, orientation) => board.placeShipOnBoard(ship, row, column, orientation),
-        attack: (opponent, row, column) => opponent.getBoard().receiveAttack(row, column), 
+        attack: (opponent, row, column) => {
+            if (isComputer) {
+                let attackCord;
+                do {
+                    row = rowGenerator();
+                    column = columnGenerator();
+                    attackCord = `${row},${column}`;  
+                } while (attacks.has(attackCord));  
+                
+                attacks.add(attackCord); 
+                return opponent.getBoard().receiveAttack(row, column);
+            } else {
+                return opponent.getBoard().receiveAttack(row, column);  
+            }
+        }, 
         getBoard: () => board,
-    }
+    };
 }
-
-
-
-
 
 function rowGenerator (){
 let hitRow = Math.floor(Math.random() * 10); 
